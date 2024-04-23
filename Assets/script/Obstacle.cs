f765bb2b939d75491af88a9ec66ea6a7c96c3260
Yenthe Van den Eynden <yenthe6@live.be>
2024-04-23 14:39:50 +0200

    eeste commit

---
+++ Assets/script/Obstacle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float Movespeed = 3.5f;
+
+    private void Update()
+    {
+        this.transform.Translate(Vector3.back * Movespeed * Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("WallEnd") == true)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
